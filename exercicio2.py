de8437c4cb7defbed0ece2b4a501d515c8dd4dc1
anitadaudt <anitadaudt@gmail.com>
2025-02-27 16:54:28 -0300

    Add files via upload

---
+++ exercicio2.py
@@ -0,0 +1,21 @@
+"""
+Exercício 2
+Autor: Anita Daudt
+Data: 27/02/2025
+Versão 0.0.1
+"""
+
+# Alocação de memória:
+
+numero_2 = 2
+numero_3 = 3
+
+# Entrada de dados:
+
+# Processamento de dados:
+
+resultado = numero_2 + numero_3
+
+# Saída de dados
+
+print(resultado)
